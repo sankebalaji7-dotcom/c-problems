8473de9ff1f79157b8502d81317c7a888cb1d11c
sankebalaji7-dotcom <sankebalaji7@gmail.com>
2026-02-23 20:25:51 +0530

    Add files via upload

---
+++ 23022026/IF ELSE P2_13.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a+b>c && a+c>b && b+c>a)
+
+    if(a==b &&b==c){
+        printf("Equilateral triangle\n");
+    }
+    else if(a==b && b==c && c==a){
+        printf("Isosceles triangle\n");
+    }
+    else{
+        printf("Scalene triangle\n");
+    }
+    else{
+        printf("Not a valid triangle\n");
+    }
+    
+    return 0;
+}
